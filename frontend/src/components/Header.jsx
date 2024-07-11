97cbe037d4146ebb6e30508867fd418a51884a8e
anthonytkool <anthonytcool@hotmail.com>
2024-07-11 18:05:58 +0700

    Finished Backend completely

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
                   />
                   <b>Cart</b>
                   {cartItems.length > 0 && (
-                    <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                    <Badge pill bg='danger' style={{ marginLeft: '5px' }}>
                       {cartItems.reduce((a, c) => a + c.qty, 0)}
                     </Badge>
                   )}
