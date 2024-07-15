17384f7875285cace9b31f32099254398694c13a
anthonytkool <anthonytcool@hotmail.com>
2024-07-15 19:40:55 +0700

    latest update

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,14 +2,14 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: '10d',
+    expiresIn: '1d',
   });
   //Set JWT as HTTP-Only cookie
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
     sameSite: 'strict',
-    maxAge: 10 * 24 * 60 * 60 * 1000, // 10 Days
+    maxAge: +24 * 60 * 60 * 1000, // 1 Days + 24 * 60 * 60 * 1000;
   });
 };
 export default generateToken;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,10 +11,15 @@ import store from './store.js';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
+import PrivateRoute from './components/PrivateRoute';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen.jsx';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen.jsx';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +28,13 @@ const router = createBrowserRouter(
       <Route path='/product/:id' element={<ProductScreen />} />
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} />
+        <Route path='/placeorder' element={<PlaceOrderScreen />} />
+      </Route>
+      ;
     </Route>
   )
 );
